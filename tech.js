613e8687014d1262be26fc01da047a7d7908fa38
yohann-d <yohann.desouza@live.fr>
2020-03-13 14:26:32 +0100

    premier

---
+++ tech.js
@@ -0,0 +1,10 @@
+/* navbar*/
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "Topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "Topnav";
+    }
+  }
